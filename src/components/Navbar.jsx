476721b6a0420ce748d9a10cf0d1f2d1865c461b
Yossef <yamar8@gmail.com>
2025-02-06 18:11:14 +0200

    more improvment in Rtl

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,9 +74,8 @@ const Navbar = () => {
             </Link> 
             <img onClick={()=>setVisible(true)}  src={i18next.dir()==="rtl"?assets.menu_icon_rtl:assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" /> 
       </div>
-                    {console.log(i18next.dir())}
         {/* Sidebar menu for small screens */}
-        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+        <div className={`absolute top-0 ${i18next.dir()==="rtl"? 'left-0' : 'right-0'} bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-2/3' : 'w-0'}`}>
                 <div className='flex flex-col text-gray-600'>
                     <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
                         <img className='h-4 rotate-180' src={i18next.dir()==="rtl"?assets.dropdown_icon_rtl:assets.dropdown_icon} alt="" />
